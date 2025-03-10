b15c3b2786fc60841921dbc950f0e5d058446192
Abdulrhman Mohamed <138893669+AYeagerr@users.noreply.github.com>
2025-03-10 22:42:51 +0200

    Delete JS/Capsule animation.js

--- JS/Capsule animation.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-                const capsule = document.querySelector(".capsule");
-                capsule.classList.add("bounce-animation");
-            });
